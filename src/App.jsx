78da8fb18f98a3aafd6dcfe13d9bec138e177f0a
Miten Chauhan <miten@chauhan.nz>
2021-02-02 21:58:18 +1300

    Rename files

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import './App.css';
+import Pixel from './Components/Pixel/Pixel';
 
 function App() {
   return (
     <div className="App">
-
+      <Pixel red={8} green={8} blue={8} />
     </div>
   );
 }
